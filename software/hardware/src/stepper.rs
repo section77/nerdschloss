361127f716bd0cdff2d5e218da4bc23bdaee8345
Florian Bruder <florian.bruder@posteo.de>
2023-01-23 22:17:07 +0100

    Change gpio variable names

--- software/hardware/src/stepper.rs
+++ software/hardware/src/stepper.rs
@@ -18,30 +18,30 @@ pub enum Direction {
 ))]
 pub fn do_steps(direction: Direction) {
     // set motor direction
-    let mut direction_gpio = Gpio::new().unwrap().get(24).unwrap().into_output();
+    let mut motor_direction_gpio = Gpio::new().unwrap().get(24).unwrap().into_output();
     match direction {
-        ::core => direction_gpio.set_high(),
-        ::clone => direction_gpio.set_low(),
+        ::core => motor_direction_gpio.set_high(),
+        ::clone => motor_direction_gpio.set_low(),
     }
 
-    let mut stepper = Gpio::new().unwrap().get(23).unwrap().into_output();
-    let mut stepper_driver = Gpio::new().unwrap().get(25).unwrap().into_output();
+    let mut motor_gpio = Gpio::new().unwrap().get(23).unwrap().into_output();
+    let mut motor_driver_gpio = Gpio::new().unwrap().get(25).unwrap().into_output();
 
     const STEPPS: i64 = 32000;
     const PWM_SLEEP_TIME: u64 = 100;
 
     println!("Start stepper");
 
-    stepper_driver.set_low();
+    motor_driver_gpio.set_low();
 
     for _ in 1..STEPPS {
-        stepper.set_high();
+        motor_gpio.set_high();
         std::thread::sleep(std::time::Duration::from_micros(PWM_SLEEP_TIME));
-        stepper.set_low();
+        motor_gpio.set_low();
         std::thread::sleep(std::time::Duration::from_micros(PWM_SLEEP_TIME));
     }
 
-    stepper_driver.set_high();
+    motor_driver_gpio.set_high();
 
     println!("Stepper done");
 }
