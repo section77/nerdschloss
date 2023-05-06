41561a23cd85b3857720daca4cf301368669c12b
Florian Bruder <florian.bruder@posteo.de>
2023-05-06 21:01:15 +0200

    Add dependency crate clap

--- software/backend/src/main.rs
+++ software/backend/src/main.rs
@@ -1,9 +1,18 @@
+use clap::Parser;
 use poem::{listener::TcpListener, Server};
 
 use backend::setup;
 
+/// nerdschloss
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
+    // Parse command line args
+    let _args = Args::parse();
+
     // Listen for new connections
     let listener = TcpListener::bind("127.0.0.1:8080");
 
