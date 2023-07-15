732bf9cfbee24e53d87e64bed59b5a34ece6b93c
Florian Bruder <florian.bruder@posteo.de>
2023-07-15 15:21:55 +0200

    Add cfg for STATE_FILE

--- software/hardware/src/lib.rs
+++ software/hardware/src/lib.rs
@@ -6,5 +6,6 @@ pub use lock::{DorLock, DorLockState};
 pub use lockswitch::{DorLockSwitch, DorLockSwitchState, DorLockSwitchStateTrait};
 pub use motor::Direction;
 
+#[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
 const STATE_FILE: &str = "/tmp/nerdschloss";
 //const STATE_FILE: &str = "/var/run/nerdschloss";
