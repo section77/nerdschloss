989dc0b214a9847055dba276814a1d164fab3de3
Florian Bruder <florian.bruder@posteo.de>
2023-12-17 00:28:54 +0100

    Add doorswitch module

---
+++ software/hardware/src/doorswitch.rs
@@ -0,0 +1,106 @@
+#[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
+use std::{fs, io::prelude::*};
+
+#[cfg(all(
+    any(target_arch = "arm", target_arch = "aarch64"),
+    target_env = "musl",
+    target_os = "linux"
+))]
+use rppal::gpio::{Gpio, InputPin, Level};
+
+#[derive(Debug, Default, Clone, Copy)]
+pub enum DoorSwitchState {
+    #[default]
+    Locked,
+    Unlocked,
+}
+
+impl std::convert::From<bool> for DoorSwitchState {
+    fn from(b: bool) -> Self {
+        match b {
+            true => DoorSwitchState::Unlocked,
+            false => DoorSwitchState::Locked,
+        }
+    }
+}
+
+impl std::convert::From<DoorSwitchState> for bool {
+    fn from(dlss: DoorSwitchState) -> Self {
+        match dlss {
+            DoorSwitchState::Unlocked => true,
+            DoorSwitchState::Locked => false,
+        }
+    }
+}
+
+pub trait DoorSwitchStateTrait {
+    fn state(&self) -> DoorSwitchState;
+}
+
+#[derive(Debug)]
+pub struct DoorSwitch {
+    #[cfg(all(
+        any(target_arch = "arm", target_arch = "aarch64"),
+        target_env = "musl",
+        target_os = "linux"
+    ))]
+    doorswitch_gpio: InputPin,
+}
+
+impl DoorSwitch {
+    #[cfg(all(
+        any(target_arch = "arm", target_arch = "aarch64"),
+        target_env = "musl",
+        target_os = "linux"
+    ))]
+    pub fn new() -> Self {
+        Self {
+            doorswitch_gpio: Gpio::new().unwrap().get(22).unwrap().into_input_pullup(),
+        }
+    }
+
+    #[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
+    pub fn new() -> Self {
+        Self::check_state_file();
+
+        Self {}
+    }
+
+    #[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
+    fn check_state_file() {
+        if fs::metadata(super::STATE_FILE).is_err() {
+            let mut file = fs::File::create(super::STATE_FILE).unwrap();
+            write!(file, "false").unwrap();
+        }
+    }
+}
+
+impl DoorSwitchStateTrait for DoorSwitch {
+    #[cfg(all(
+        any(target_arch = "arm", target_arch = "aarch64"),
+        target_env = "musl",
+        target_os = "linux"
+    ))]
+    fn state(&self) -> DoorSwitchState {
+        match &self.doorswitch_gpio.read() {
+            Level::Low => DoorSwitchState::Locked,
+            Level::High => DoorSwitchState::Unlocked,
+        }
+    }
+
+    #[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
+    fn state(&self) -> DoorSwitchState {
+        let mut file = fs::File::open(super::STATE_FILE).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let s: bool = contents.trim().parse().unwrap();
+
+        s.into()
+    }
+}
+
+impl Default for DoorSwitch {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- software/hardware/src/lib.rs
+++ software/hardware/src/lib.rs
@@ -1,11 +1,14 @@
+mod doorswitch;
 mod lock;
 mod lockswitch;
 mod motor;
 
-pub use lock::{Lock, LockState};
-pub use lockswitch::{LockSwitch, LockSwitchState, LockSwitchStateTrait};
-pub use motor::Direction;
+pub use self::{
+    doorswitch::{DoorSwitch, DoorSwitchState, DoorSwitchStateTrait},
+    lock::{Lock, LockState},
+    lockswitch::{LockSwitch, LockSwitchState, LockSwitchStateTrait},
+    motor::Direction,
+};
 
 #[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
 const STATE_FILE: &str = "/tmp/nerdschloss";
-//const STATE_FILE: &str = "/var/run/nerdschloss";
