12f1fbc4b7c9f3390553cfe4caae285d243ee86c
Florian Bruder <florian.bruder@posteo.de>
2024-02-12 22:24:50 +0100

    Rename state files

--- software/raspberrypi/hardware/src/lib.rs
+++ software/raspberrypi/hardware/src/lib.rs
@@ -6,6 +6,6 @@ mod motor;
 pub use self::motor::Direction;
 
 #[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
-const LOCKSWITCH_STATE_FILE: &str = "/tmp/nerdschloss/lockswitch";
+const LOCKSWITCH_STATE_FILE: &str = "/tmp/nerdschloss/lockswitch_state";
 #[cfg(all(target_arch = "x86_64", any(target_os = "macos", target_os = "linux")))]
-const DOORSWITCH_STATE_FILE: &str = "/tmp/nerdschloss/doorswitch";
+const DOORSWITCH_STATE_FILE: &str = "/tmp/nerdschloss/doorswitch_state";
