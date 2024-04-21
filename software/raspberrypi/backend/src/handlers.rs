f1719bf9b2ea8a05c7203bd15e971211e0269bad
Florian Bruder <florian.bruder@posteo.de>
2024-04-22 00:08:28 +0200

    feature: Allways output debug statements

--- software/raspberrypi/backend/src/handlers.rs
+++ software/raspberrypi/backend/src/handlers.rs
@@ -6,20 +6,17 @@ use hardware::Direction;
 
 #[handler]
 pub async fn state() -> impl IntoResponse {
-    #[cfg(debug_assertions)]
     debug!("state");
 }
 
 #[handler]
 pub async fn open(sender: Data<&Sender<Direction>>) -> impl IntoResponse {
-    #[cfg(debug_assertions)]
     debug!("open");
     sender.0.send(Direction::Open).await.unwrap();
 }
 
 #[handler]
 pub async fn close(sender: Data<&Sender<Direction>>) -> impl IntoResponse {
-    #[cfg(debug_assertions)]
     debug!("close");
     sender.0.send(Direction::Close).await.unwrap();
 }
