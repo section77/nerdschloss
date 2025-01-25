e97e99478a2cd81a45de5d7e7d068edc6b0e0a44
Florian Bruder <florian.bruder@posteo.de>
2025-01-25 22:57:02 +0100

    feature: Add python script to set MatterMost status
    - Add python script
    - Run python script from rust

--- software/raspberrypi/backend/src/mattermost/mod.rs
+++ software/raspberrypi/backend/src/mattermost/mod.rs
@@ -1,6 +1,7 @@
 pub mod configuration;
 
-// mod client;
+mod client;
+// mod types;
 
 use std::process::Command;
 
@@ -8,10 +9,13 @@ use configuration::MatterMost;
 
 pub fn mattermost(configuration: &MatterMost, state: bool) {
     if configuration.enable {
-        // let code = std::include_str!("../../backend/python/mattermost.py");
-        let _cmd = Command::new("./ve/bin/python")
+        let code = std::include_str!("../../../backend/python/mattermost.py");
+        let output = Command::new("python3")
+            // let output = Command::new("./ve/bin/python3")
             .args([
-                "./backend/python/mattermost.py ",
+                // "python/mattermost.py",
+                "-c",
+                code,
                 configuration.url.as_str(),
                 configuration.loginid.as_str(),
                 configuration.apitoken.as_str(),
@@ -21,6 +25,12 @@ pub fn mattermost(configuration: &MatterMost, state: bool) {
             ])
             .output()
             .expect("Error: Failed to set MatterMost message");
+
+        println!("output start");
+        use std::io::{self, Write};
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+        println!("output end");
     }
 }
 
@@ -28,7 +38,7 @@ pub fn mattermost(configuration: &MatterMost, state: bool) {
 mod tests {
     use wiremock::{
         http::Method,
-        matchers::{any, method},
+        matchers::{body_json, header, method, path},
         {Mock, MockServer, ResponseTemplate},
     };
 
@@ -40,7 +50,7 @@ mod tests {
     async fn mattermost_disabled() {
         let mock_server = MockServer::start().await;
 
-        Mock::given(method(Method::PUT))
+        Mock::given(method(Method::GET))
             .respond_with(ResponseTemplate::new(200))
             .expect(0)
             .mount(&mock_server)
@@ -53,25 +63,39 @@ mod tests {
         mattermost(&mm_config, true);
     }
 
-    #[ignore = "TODO"]
-    #[tokio::test]
-    async fn send_mattermost_message() {
-        let mock_server = MockServer::start().await;
+    // #[tokio::test]
+    // async fn send_mattermost_message() {
+    //     let mock_server = MockServer::start().await;
 
-        Mock::given(any())
-            .respond_with(ResponseTemplate::new(200))
-            .expect(1)
-            .mount(&mock_server)
-            .await;
+    //     let user_responce = super::types::user::UserResponce {
+    //         id: "bla".to_string(),
+    //         ..Default::default()
+    //     };
+    //     Mock::given(method(Method::GET))
+    //         .and(path("api/v4/users/me"))
+    //         .and(header("content-type", "application/json"))
+    //         .and(body_json(&user_responce))
+    //         .respond_with(ResponseTemplate::new(200))
+    //         .expect(1)
+    //         .mount(&mock_server)
+    //         .await;
 
-        let mm_config = MatterMost {
-            enable: true,
-            url: mock_server.uri(),
-            loginid: String::new(),
-            apitoken: String::new(),
-            scheme: String::from("http"),
-            port: mock_server.address().port(),
-        };
-        mattermost(&mm_config, true);
-    }
+    //     // Mock::given(method(Method::GET))
+    //     //     .and(path("api/v4/teams/name/section77"))
+    //     //     .and(bearer_token(""))
+    //     //     .respond_with(ResponseTemplate::new(200))
+    //     //     .expect(1)
+    //     //     .mount(&mock_server)
+    //     //     .await;
+
+    //     let mm_config = MatterMost {
+    //         enable: true,
+    //         url: mock_server.address().ip().to_string(),
+    //         loginid: String::from("clubstatus"),
+    //         apitoken: String::from("apitoken"),
+    //         scheme: String::from("http"),
+    //         port: mock_server.address().port(),
+    //     };
+    //     mattermost(&mm_config, true);
+    // }
 }
