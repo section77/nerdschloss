e7dafa80d55142064ac2762095b244e30f177e02
Jan Alexander <jan@nalx.net>
2025-04-24 23:57:36 +0200

    spaceapi: add CORS header and cleanup response

--- software/spaceapi_backend/index.php
+++ software/spaceapi_backend/index.php
@@ -3,5 +3,6 @@
 $doorStatus = file_get_contents('door_status.txt');
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
-echo '{"api":"0.13","api_compatibility":["14"],"space":"section77","logo":"https://section77.de/static/section77_logo_vector.svg","url":"https://section77.de/","location":{"address":"Hauptstraße 1, 77652 Offenburg, Germany","lat":48.4771,"lon":7.9461},"contact":{"twitter":"@section77de","email":"info@section77.de","mastodon":"@section77@chaos.social"},"feeds":{"calendar":{"type":"ical","url":"https://section77.de/section77.ics"}},"issue_report_channels":["email"],"state":{"open":' . ($doorStatus === 'open' ? 'true' : 'false') . '},"sensors":{"people_now_present":[{"location":"Gleis0","value":0}]},"ext_ccc":"chaostreff","versions":{"spaceapi-rs":"0.9.0","spaceapi-server-rs":"0.8.0"}}';
+echo '{"api":"0.13","api_compatibility":["14"],"space":"section77","logo":"https://section77.de/static/section77_logo_vector.svg","url":"https://section77.de/","location":{"address":"Hauptstraße 1, 77652 Offenburg, Germany","lat":48.4771,"lon":7.9461},"contact":{"twitter":"@section77de","email":"info@section77.de","mastodon":"@section77@chaos.social"},"feeds":{"calendar":{"type":"ical","url":"https://section77.de/section77.ics"}},"issue_report_channels":["email"],"state":{"open":' . ($doorStatus === 'open' ? 'true' : 'false') . '},"ext_ccc":"chaostreff"}';
