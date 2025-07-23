ebf044289fb9b652f179950ce4c2b6414485665c
Florian Bruder <florian.bruder@posteo.de>
2025-07-23 19:00:40 +0200

    refactor : Reenabled setting the MatterMost state via Python script

--- software/raspberrypi/backend/src/mattermost/mod.rs
+++ software/raspberrypi/backend/src/mattermost/mod.rs
@@ -1,144 +1,155 @@
 pub mod configuration;
 
-use mattermost_rust_client::{apis, models};
+// use mattermost_rust_client::{apis, models};
 use tracing::{error, info};
 
 use configuration::MatterMost;
+
+// pub async fn mattermost(configuration: &MatterMost, state: bool) {
+//     if !configuration.enable {
+//         return;
+//     }
+
+//     let url = configuration.url.clone();
+//     let _login_id = configuration.loginid.clone();
+//     let api_token = configuration.apitoken.clone();
+//     let scheme = configuration.scheme.clone();
+//     let port: u16 = configuration.port;
+
+//     // Create configuration
+//     let mut config = apis::configuration::Configuration::new();
+//     config.base_path = format!("{scheme}://{url}/api/v4");
+//     let api_key = apis::configuration::ApiKey {
+//         prefix: None,
+//         key: api_token,
+//     };
+//     config.api_key = Some(api_key);
+
+//     let _user = match apis::users_api::get_user(&config, "me").await {
+//         Ok(user) => user,
+//         Err(e) => {
+//             error!("Error: {e}");
+//             return;
+//         }
+//     };
+
+//     // Get team by name "section77"
+//     let team = match apis::teams_api::get_team_by_name(&config, "section77").await {
+//         Ok(team) => team,
+//         Err(e) => {
+//             error!("Error: {e}");
+//             return;
+//         }
+//     };
+//     let team_id = match team.id.as_ref().ok_or("Team ID is missing") {
+//         Ok(team_id) => team_id,
+//         Err(e) => {
+//             error!("Error: {e}");
+//             return;
+//         }
+//     };
+
+//     // Get channel by name "clubstatus"
+//     let channel =
+//         match apis::channels_api::get_channel_by_name(&config, team_id, "clubstatus", None).await {
+//             Ok(channel) => channel,
+//             Err(e) => {
+//                 error!("Error: {e}");
+//                 return;
+//             }
+//         };
+//     let channel_id = match channel.id.as_ref().ok_or("Channel ID is missing") {
+//         Ok(channel_id) => channel_id,
+//         Err(e) => {
+//             error!("Error: {e}");
+//             return;
+//         }
+//     };
+
+//     // Determine message and display name based on state
+//     let (message, display_name) = match &state {
+//         true => ("TEST: Die Section77 ist offen", "Status: Offen"),
+//         false => ("TEST: Die Section77 ist geschlossen", "Status: Geschlossen"),
+//     };
+
+//     // Create post
+//     let post_request = models::CreatePostRequest::new(channel_id.clone(), message.to_string());
+
+//     match apis::posts_api::create_post(&config, post_request, None).await {
+//         Ok(_) => {
+//             info!("Post created");
+//         }
+//         Err(e) => {
+//             error!("Error: {e}");
+//             return;
+//         }
+//     }
+
+//     // Update channel display name
+//     let patch_channel = models::PatchChannelRequest {
+//         display_name: Some(display_name.to_string()),
+//         ..Default::default()
+//     };
+
+//     match apis::channels_api::patch_channel(&config, channel_id, patch_channel).await {
+//         Ok(_) => {
+//             info!("Channel updated");
+//         }
+//         Err(e) => {
+//             error!("Error: {e}");
+//         }
+//     }
+
+//     match apis::users_api::logout(&config).await {
+//         Ok(_) => {
+//             info!("Logout successful");
+//         }
+//         Err(e) => {
+//             error!("Error: {e}");
+//         }
+//     }
+// }
 
 pub async fn mattermost(configuration: &MatterMost, state: bool) {
     if !configuration.enable {
         return;
     }
 
-    let url = configuration.url.clone();
-    let _login_id = configuration.loginid.clone();
-    let api_token = configuration.apitoken.clone();
-    let scheme = configuration.scheme.clone();
-    let port: u16 = configuration.port;
+    let code = std::include_str!("../../../backend/python/mattermost.py");
 
-    // Create configuration
-    let mut config = apis::configuration::Configuration::new();
-    config.base_path = format!("{scheme}://{url}:{port}/api/v4");
-    let api_key = apis::configuration::ApiKey {
-        prefix: None,
-        key: api_token,
-    };
-    config.api_key = Some(api_key);
-
-    let _user = match apis::users_api::get_user(&config, "me").await {
-        Ok(user) => user,
+    #[allow(unused_variables)]
+    let output = match std::process::Command::new(&configuration.python)
+        .args([
+            "-c",
+            code,
+            configuration.url.as_str(),
+            configuration.loginid.as_str(),
+            configuration.apitoken.as_str(),
+            configuration.scheme.as_str(),
+            &configuration.port.to_string(),
+            &state.to_string(),
+        ])
+        .output()
+    {
+        Ok(output) => {
+            info!("Successfully executed MatterMost Python script");
+            output
+        }
         Err(e) => {
-            error!("Error: {e}");
+            error!("Failed to execute MatterMost Python script: {e}");
             return;
         }
     };
 
-    // Get team by name "section77"
-    let team = match apis::teams_api::get_team_by_name(&config, "section77").await {
-        Ok(team) => team,
-        Err(e) => {
-            error!("Error: {e}");
-            return;
-        }
-    };
-    let team_id = match team.id.as_ref().ok_or("Team ID is missing") {
-        Ok(team_id) => team_id,
-        Err(e) => {
-            error!("Error: {e}");
-            return;
-        }
-    };
-
-    // Get channel by name "clubstatus"
-    let channel =
-        match apis::channels_api::get_channel_by_name(&config, team_id, "clubstatus", None).await {
-            Ok(channel) => channel,
-            Err(e) => {
-                error!("Error: {e}");
-                return;
-            }
-        };
-    let channel_id = match channel.id.as_ref().ok_or("Channel ID is missing") {
-        Ok(channel_id) => channel_id,
-        Err(e) => {
-            error!("Error: {e}");
-            return;
-        }
-    };
-
-    // Determine message and display name based on state
-    let (message, display_name) = match &state {
-        true => ("Die Section77 ist offen", "Status: Offen"),
-        false => ("Die Section77 ist geschlossen", "Status: Geschlossen"),
-    };
-
-    // Create post
-    let post_request = models::CreatePostRequest::new(channel_id.clone(), message.to_string());
-
-    match apis::posts_api::create_post(&config, post_request, None).await {
-        Ok(_) => {
-            info!("Post created");
-        }
-        Err(e) => {
-            error!("Error: {e}");
-            return;
-        }
-    }
-
-    // Update channel display name
-    let patch_channel = models::PatchChannelRequest {
-        display_name: Some(display_name.to_string()),
-        ..Default::default()
-    };
-
-    match apis::channels_api::patch_channel(&config, channel_id, patch_channel).await {
-        Ok(_) => {
-            info!("Channel updated");
-        }
-        Err(e) => {
-            error!("Error: {e}");
-        }
-    }
-
-    match apis::users_api::logout(&config).await {
-        Ok(_) => {
-            info!("Logout successful");
-        }
-        Err(e) => {
-            error!("Error: {e}");
-        }
+    #[cfg(test)]
+    {
+        println!("output start");
+        println!("Script stdout: {}", String::from_utf8_lossy(&output.stdout));
+        println!("Script stderr: {}", String::from_utf8_lossy(&output.stderr));
+        println!("Script exit code: {}", output.status);
+        println!("output end");
     }
 }
-
-// pub fn mattermost(configuration: &MatterMost, state: bool) {
-//     use std::process::Command;
-//     if configuration.enable {
-//         let code = std::include_str!("../../../backend/python/mattermost.py");
-//         #[allow(unused_variables)]
-//         let output = Command::new(&configuration.python)
-//             .args([
-//                 "-c",
-//                 code,
-//                 configuration.url.as_str(),
-//                 configuration.loginid.as_str(),
-//                 configuration.apitoken.as_str(),
-//                 configuration.scheme.as_str(),
-//                 &configuration.port.to_string(),
-//                 &state.to_string(),
-//             ])
-//             .output()
-//             .expect("Error: Failed to set MatterMost message");
-
-//         #[cfg(test)]
-//         {
-//             println!("output start");
-//             println!("Script stdout: {}", String::from_utf8_lossy(&output.stdout));
-//             println!("Script stderr: {}", String::from_utf8_lossy(&output.stderr));
-//             println!("Script exit code: {}", output.status);
-//             println!("output end");
-//         }
-//     }
-// }
 
 #[cfg(test)]
 mod tests {
@@ -225,6 +236,7 @@ mod tests {
         Mock::given(method(Method::PUT))
             .and(path("/api/v4/channels/channel-id/patch"))
             .and(body_json(json!({
+                "id": "channel-id",
                 "display_name": "Status: Offen"
             })))
             .respond_with(ResponseTemplate::new(200).set_body_json(json!({
@@ -313,10 +325,11 @@ mod tests {
         Mock::given(method(Method::PUT))
             .and(path("/api/v4/channels/channel-id/patch"))
             .and(body_json(json!({
+                "id": "channel-id",
                 "display_name": "Status: Geschlossen"
             })))
             .respond_with(ResponseTemplate::new(200).set_body_json(json!({
-                "id": "channel-id",
+
                 "display_name": "Status: Geschlossen"
             })))
             .expect(1)
