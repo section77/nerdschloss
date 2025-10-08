48858767c1e643b3ce89cfc7d7c13ebc57f085cf
Florian Bruder <florian.bruder@posteo.de>
2025-10-08 14:41:58 +0200

    feature: Add MPV mqtt support

--- software/raspberrypi/backend/src/notifyer.rs
+++ software/raspberrypi/backend/src/notifyer.rs
@@ -2,11 +2,30 @@ use tokio::sync::mpsc::Receiver;
 
 use crate::{configuration::ConfigurationRef, mattermost::mattermost, mqtt, spaceapi::spaceapi};
 
-pub async fn notify(configuration: ConfigurationRef, mut receiver: Receiver<bool>) {
-    while let Some(state) = receiver.recv().await {
-        let mqtt = mqtt::mqtt(&configuration.mqtt, state);
+pub async fn notify(configuration: ConfigurationRef, mut notify_receiver: Receiver<bool>) {
+    let mqtt_notifier = mqtt::MqttNotifier::new_with_configuration(&configuration.mqtt);
+
+    while let Some(state) = notify_receiver.recv().await {
         let mattermost = mattermost(&configuration.mattermost, state);
         let spaceapi = spaceapi(&configuration.spaceapi, state);
-        tokio::join!(mqtt, mattermost, spaceapi);
+
+        mqtt_notifier.notify(state);
+        tokio::join!(mattermost, spaceapi);
     }
+}
+
+pub trait Notifier {
+    type Configuration;
+
+    #[must_use]
+    fn new_with_configuration(configuration: &Self::Configuration) -> Self;
+    fn notify(&self, state: bool);
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+pub enum NotifierState {
+    Setting,
+    Succeeded,
+    Failed,
 }
